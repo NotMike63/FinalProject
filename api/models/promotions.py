6709ca4abea6d6f3e8db6d60e85892bb8ba81cc5
Michael Spitz <mspitz2005@gmail.com>
2025-04-14 10:41:19 -0400

    Fix: Orders correction.

--- api/models/promotions.py
+++ api/models/promotions.py
@@ -6,6 +6,5 @@ from ..dependencies.database import Base
 class Promotion(Base):
     __tablename__ = "promotions"
 
-    tracking_number = Column(Integer, ForeignKey("orders.tracking_number"), primary_key=True)
     code = Column(String(100), unique=True, nullable=True)
     expiration_date = Column(String(100), nullable=True)
