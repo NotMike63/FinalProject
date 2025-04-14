6ba0283c5a572e71a1769b5da6e90f47375f7d02
Nari6745 <hrhfhhdhf62@gmail.com>
2025-04-14 14:54:27 -0400

    Update resource_managements.py

--- api/models/resource_managements.py
+++ api/models/resource_managements.py
@@ -11,4 +11,4 @@ class ResourceManagement(Base):
     item = Column(String(100), unique=True, nullable=False)
     amount = Column(Integer, index=True, nullable=False, server_default='0.0')
 
-    recipes = relationship("Recipe", back_populates="resource_managments")
+    recipes = relationship("Recipe", back_populates="resource_managements")
