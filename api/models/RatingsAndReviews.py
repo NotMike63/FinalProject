2c67ed48f5e281d381ba5799e6418dd92a472de2
Michael Spitz <mspitz2005@gmail.com>
2025-04-18 11:16:25 -0400

    Fix: reinserted ForeignKeys after fixing db issue.

--- api/models/RatingsAndReviews.py
+++ api/models/RatingsAndReviews.py
@@ -7,6 +7,6 @@ from ..dependencies.database import Base
 class RatingsAndReviews(Base):
     __tablename__ = "RatingsAndReviews"
 
-    tracking_number = Column(Integer, primary_key=True, index=True, autoincrement=True) # ForeignKey("orders.order_id")
+    tracking_number = Column(Integer, ForeignKey("orders.order_id"), primary_key=True, index=True, autoincrement=True)
     review_text = Column(String(500), nullable=False)
     review_score = Column(Float, nullable=False)
