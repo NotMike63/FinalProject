bc7a4ea95cb6ba6d6f72f541d1ccbf0019ba832a
adithyanair7 <anair21@charlotte.edu>
2025-04-25 16:15:12 -0400

    Update RatingsAndReviews.py

--- api/models/RatingsAndReviews.py
+++ api/models/RatingsAndReviews.py
@@ -5,7 +5,7 @@ from ..dependencies.database import Base
 
 class RatingsAndReviews(Base):
     __tablename__ = "RatingsAndReviews"
-
+    menu_item_id = Column(Integer, ForeignKey("menu_items.menu_item_id"), nullable=False)
     order_id = Column(Integer, ForeignKey("orders.order_id"), primary_key=True, index=True, autoincrement=True)
     review_text = Column(String(500), nullable=False)
     review_score = Column(Float, nullable=False)
